34e3604237d001002aaa92a2d900373a58769c12
11292-stella <stella.marucelli@gmail.com>
2025-02-20 16:43:46 +0100

    seconda parte

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+const problemibot = function (e) {}
+
+const creazioneCella = function (numbcell) {
+  const tabellone = document.getElementById("tabellone")
+  for (let i = 0; i < numbcell; i++) {
+    const cella = document.createElement("div")
+    cella.classList.add("celle")
+    cella.innerText = ""
+
+    tabellone.appendChild(cella)
+  }
+}
+
+const generanumb = function (e) {
+  e.preventDefault()
+  const celle1 = document.querySelectorAll(".celle")
+  const numerocas = Math.floor(Math.random() * 26 + 1)
+  let cellaFree = null
+  for (let i = 0; i < celle1.length; i++) {
+    if (cellaFree === null && celle1[i].innerText === "") {
+      cellaFree = celle1[i]
+    }
+  }
+  if (cellaFree) {
+    cellaFree.innerText = numerocas
+  } else {
+    return "celle piene"
+  }
+}
+creazioneCella(26)
+document.getElementById("generaNumb").addEventListener("click", generanumb)
